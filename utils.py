856eaf3281512aa5f6e6ffb02c0654cf98a17690
Deshan Samarathunga <120622984+HNDeshanSamarathunga@users.noreply.github.com>
2025-04-16 16:47:39 +0530

    app.py

---
+++ utils.py
@@ -0,0 +1,35 @@
+# utils.py
+
+emotion_to_youtube = {
+    'happy': [
+        "https://www.youtube.com/watch?v=ZbZSe6N_BXs",  # Happy - Pharrell
+        "https://www.youtube.com/watch?v=9bZkp7q19f0",  # Gangnam Style
+        "https://www.youtube.com/watch?v=OPf0YbXqDm0"   # Uptown Funk
+    ],
+    'sad': [
+        "https://www.youtube.com/watch?v=RgKAFK5djSk",  # See You Again
+        "https://www.youtube.com/watch?v=450p7goxZqg",  # Let Her Go
+        "https://www.youtube.com/watch?v=hT_nvWreIhg"   # Say Something
+    ],
+    'angry': [
+        "https://www.youtube.com/watch?v=04F4xlWSFh0",  # Linkin Park - Numb
+        "https://www.youtube.com/watch?v=QwZT7T-TXT0",  # Eminem - Not Afraid
+        "https://www.youtube.com/watch?v=kXYiU_JCYtU"   # Linkin Park - Numb Encore
+    ],
+    'surprise': [
+        "https://www.youtube.com/watch?v=LsoLEjrDogU",  # Justin Timberlake
+        "https://www.youtube.com/watch?v=fLexgOxsZu0",  # Bruno Mars - Treasure
+    ],
+    'fear': [
+        "https://www.youtube.com/watch?v=8SbUC-UaAxE",  # November Rain
+        "https://www.youtube.com/watch?v=uelHwf8o7_U"   # Love The Way You Lie
+    ],
+    'disgust': [
+        "https://www.youtube.com/watch?v=ktvTqknDobU",  # Imagine Dragons - Radioactive
+        "https://www.youtube.com/watch?v=CevxZvSJLk8"   # Katy Perry - Roar
+    ],
+    'neutral': [
+        "https://www.youtube.com/watch?v=YQHsXMglC9A",  # Adele - Hello
+        "https://www.youtube.com/watch?v=JGwWNGJdvx8"   # Ed Sheeran - Shape of You
+    ]
+}
